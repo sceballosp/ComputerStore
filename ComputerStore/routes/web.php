b668e1dcce32f0abe45dedc0f89a1bcdd0799a9c
jpmadridf <jpmadridf@eafit.edu.co>
2022-03-22 15:28:10 -0500

    Modificacion ruta

--- ComputerStore/routes/web.php
+++ ComputerStore/routes/web.php
@@ -15,8 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 
-Route::get('/categories/create', 'App\Http\Controllers\CategoryController@create')->name("category.create");
-Route::post('/categories', 'App\Http\Controllers\CategoryController@save');
+
 
 Route::get('/computers', 'App\Http\Controllers\ComputerController@index')->name("computer.index");
 Route::post('/computers', 'App\Http\Controllers\ComputerController@save');
@@ -28,6 +27,9 @@ Route::post('/orders', 'App\Http\Controllers\OrderController@save');
 Route::get('/orders/create', 'App\Http\Controllers\OrderController@create')->name("order.create");
 Route::get('/orders/{id}', 'App\Http\Controllers\OrderController@show')->name("order.show");
 
-
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('/categories/create', 'App\Http\Controllers\CategoryController@create')->name("category.create");
+    Route::post('/categories', 'App\Http\Controllers\CategoryController@save');
+});
 Auth::routes();
 
